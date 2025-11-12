ec9d0e2aae0e48319e058b042866fd264beb7df7
Dan Rosenstark <github@dr2050.com>
2025-11-12 12:31:39 -0500

    tests set up

---
+++ HelloWorldTests/SnapshotTests.swift
@@ -0,0 +1,19 @@
+// (c) Confusion Studios LLC and affiliates. Confidential and proprietary.
+
+import XCTest
+import SnapshotTesting
+@testable import HelloWorld
+
+class SnapshotTests: XCTestCase {
+
+    func testViewControllerSnapshot() {
+        let vc = ViewController()
+        vc.view.frame = CGRect(x: 0, y: 0, width: 393, height: 852) // iPhone 15 Pro size
+
+        // Trigger viewDidLoad and layout
+        vc.loadViewIfNeeded()
+        vc.view.layoutIfNeeded()
+
+        assertSnapshot(of: vc, as: .image, record: true)
+    }
+}
