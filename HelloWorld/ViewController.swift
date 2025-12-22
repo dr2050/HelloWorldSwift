fe9c31fe307894c149404b799452ed369820781f
Dan Rosenstark <github@dr2050.com>
2025-12-22 12:35:25 -0500

    Clean

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -4,28 +4,5 @@ class ViewController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let root = UIViewController()
-        root.view.backgroundColor = .orange
-        root.title = "Main"
-
-        let button = UIBarButtonItem(
-            title: "Next",
-            style: .plain,
-            target: self,
-            action: #selector(showAnother)
-        )
-
-        root.navigationItem.rightBarButtonItem = button
-        viewControllers = [root]
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            self.showAnother()
-        }
-    }
-
-    @objc private func showAnother() {
-        let vc = TutorialController(slug: "ipad")
-        pushViewController(vc, animated: true)
     }
 }
